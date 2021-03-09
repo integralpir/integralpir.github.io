a2e00cb5253f7c76317eeee4b93c5ed940bd5bb7
integralpir <roma_5712@mail.ru>
2021-03-09 13:20:55 +0500

    09.03.21

---
+++ CSS/CSS_app.js
@@ -0,0 +1,4 @@
+function toggleTheme(){
+    document.querySelector(".up").classList.toggle("nextTheme");
+    document.querySelector(".bot").classList.toggle("nextTheme")
+}
---
+++ HTML4/HTML4_app.js
@@ -0,0 +1,3 @@
+function End() {
+    document.querySelector(".main").classList.toggle("dark");
+}
